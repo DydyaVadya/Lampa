25d292eceaa7f8f6c7006384d9c27a01319dcd4d
dydyaVadya <39806728+DydyaVadya@users.noreply.github.com>
2026-01-12 15:46:08 +0200

    Update ua_m3u_tv.js

--- ua_m3u_tv.js
+++ ua_m3u_tv.js
@@ -1,206 +1,85 @@
 (function() {
     'use strict';
 
-    // Чекаємо завантаження Lampa
-    function startPlugin() {
-        if (!window.Lampa) {
-            setTimeout(startPlugin, 300);
-            return;
+    console.log('=== UA IPTV: Starting plugin ===');
+
+    function waitLampa(callback) {
+        if (window.Lampa && Lampa.Component && Lampa.Activity) {
+            console.log('UA IPTV: Lampa detected');
+            callback();
+        } else {
+            console.log('UA IPTV: Waiting for Lampa...');
+            setTimeout(function() { waitLampa(callback); }, 500);
         }
+    }
 
-        var manifest = {
-            type: 'video',
-            version: '1.0.2',
-            name: 'UA IPTV',
-            description: 'Ukrainian IPTV channels from mater.com.ua',
-            component: 'ua_iptv',
-            icon: '<svg width="36" height="36" viewBox="0 0 36 36"><rect width="36" height="36" rx="4" fill="white" opacity="0.3"/></svg>'
-        };
-
-        var M3U_URL = 'https://mater.com.ua/ip/ua.m3u';
-
-        // Реєструємо плагін
-        Lampa.Manifest.plugins = manifest;
-
-        // Додаємо в меню при старті
-        function addToMenu() {
-            var item = $('<li class="menu__item selector" data-action="ua_iptv"><div class="menu__ico"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="24" height="24" rx="2" fill="currentColor"/><path d="M6 8h12v2H6V8zm0 4h12v2H6v-2zm0 4h12v2H6v-2z" fill="white"/></svg></div><div class="menu__text">UA IPTV</div></li>');
-
-            $('.menu .menu__list').eq(0).append(item);
-
-            item.on('hover:enter', function() {
-                Lampa.Activity.push({
-                    url: '',
-                    title: 'UA IPTV',
-                    component: 'ua_iptv',
-                    page: 1
-                });
-            });
-        }
-
-        // Парсинг M3U
-        function parseM3U(text) {
-            var lines = text.split('\n');
-            var channels = [];
-            var current = null;
-
-            for (var i = 0; i < lines.length; i++) {
-                var line = lines[i].trim();
-
-                if (line.startsWith('#EXTINF')) {
-                    var groupMatch = line.match(/group-title="([^"]+)"/);
-                    var logoMatch = line.match(/tvg-logo="([^"]+)"/);
-                    var nameMatch = line.match(/,(.*)$/);
-
-                    current = {
-                        title: nameMatch ? nameMatch[1].trim() : 'Channel',
-                        group: groupMatch ? groupMatch[1] : 'Загальні',
-                        logo: logoMatch ? logoMatch[1] : ''
-                    };
-                } else if (line.startsWith('http') && current) {
-                    current.url = line;
-                    channels.push(current);
-                    current = null;
-                }
-            }
-
-            return channels;
-        }
-
-        function groupChannels(channels) {
-            var groups = {};
-            channels.forEach(function(ch) {
-                var g = ch.group || 'Загальні';
-                if (!groups[g]) groups[g] = [];
-                groups[g].push(ch);
-            });
-            return groups;
-        }
-
-        // Компонент
-        function component(object) {
-            var network = new Lampa.Reguest();
-            var scroll = new Lampa.Scroll({horizontal: false, vertical: true});
-            var items = [];
-            var html = $('<div></div>');
+    waitLampa(function() {
+        // Тестовий компонент
+        var TestComponent = function(object) {
+            var html = $('<div class="category-full"><div style="padding: 2em; text-align: center; color: white; font-size: 2em;">UA IPTV працює!</div></div>');
 
             this.create = function() {
-                var self = this;
-                this.activity.loader(true);
-
-                scroll.minus();
-                html.append(scroll.render());
-
-                network.native(M3U_URL, function(data) {
-                    self.activity.loader(false);
-
-                    try {
-                        var channels = parseM3U(data);
-                        var groups = groupChannels(channels);
-
-                        Object.keys(groups).forEach(function(groupName) {
-                            var cards = groups[groupName];
-
-                            var line = new Lampa.Line({
-                                title: groupName + ' · ' + cards.length
-                            });
-
-                            line.create = function() {
-                                cards.forEach(function(ch) {
-                                    var card = Lampa.Template.get('card', {
-                                        title: ch.title,
-                                        release_year: ''
-                                    });
-
-                                    card.addClass('card--category');
-
-                                    if (ch.logo) {
-                                        card.find('.card__img').css({
-                                            'background-image': 'url(' + ch.logo + ')',
-                                            'background-size': 'contain',
-                                            'background-position': 'center'
-                                        });
-                                    }
-
-                                    card.on('hover:enter', function() {
-                                        if (ch.url) {
-                                            Lampa.Player.play({
-                                                title: ch.title,
-                                                url: ch.url
-                                            });
-                                        }
-                                    });
-
-                                    this.append(card);
-                                }, this);
-                            };
-
-                            scroll.append(line.render());
-                            items.push(line);
-                        });
-
-                    } catch(e) {
-                        console.error('Parse error:', e);
-                        Lampa.Noty.show('Помилка парсингу M3U');
-                    }
-
-                }, function(e) {
-                    self.activity.loader(false);
-                    console.error('Load error:', e);
-                    Lampa.Noty.show('Помилка завантаження');
-                }, false, {
-                    dataType: 'text'
-                });
+                console.log('UA IPTV: Component created');
             };
 
             this.start = function() {
+                console.log('UA IPTV: Component started');
                 Lampa.Controller.add('content', {
-                    toggle: function() {
-                        Lampa.Controller.collectionSet(scroll.render());
-                        Lampa.Controller.collectionFocus(false, scroll.render());
-                    },
-                    left: function() {
-                        if (Navigator.canmove('left')) Navigator.move('left');
-                        else Lampa.Controller.toggle('menu');
-                    },
-                    up: function() {
-                        if (Navigator.canmove('up')) Navigator.move('up');
-                        else Lampa.Controller.toggle('head');
-                    },
-                    down: function() {
-                        Navigator.move('down');
-                    },
-                    right: function() {
-                        Navigator.move('right');
-                    },
+                    toggle: function() {},
                     back: function() {
                         Lampa.Activity.backward();
                     }
                 });
-
                 Lampa.Controller.toggle('content');
             };
 
             this.pause = function() {};
             this.stop = function() {};
             this.render = function() { return html; };
-            this.destroy = function() {
-                network.clear();
-                scroll.destroy();
-                html.remove();
-            };
-        }
+            this.destroy = function() { html.remove(); };
+        };
 
-        Lampa.Component.add('ua_iptv', component);
+        Lampa.Component.add('ua_iptv_test', TestComponent);
+        console.log('UA IPTV: Component registered');
 
-        // Додаємо в меню після завантаження
-        if (Lampa.Controller) {
-            setTimeout(addToMenu, 1000);
-        }
+        // Додаємо пункт меню
+        setTimeout(function() {
+            console.log('UA IPTV: Adding menu item...');
 
-        console.log('UA IPTV plugin loaded successfully');
-    }
+            var menuHtml = '<li class="menu__item selector" data-action="ua_iptv_test">' +
+                '<div class="menu__ico">' +
+                '<svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><rect width="24" height="24" rx="2"/></svg>' +
+                '</div>' +
+                '<div class="menu__text">UA IPTV TEST</div>' +
+                '</li>';
 
-    startPlugin();
+            var $menu = $('.menu .menu__list').eq(0);
+
+            if ($menu.length > 0) {
+                $menu.append(menuHtml);
+                console.log('UA IPTV: Menu item added to', $menu);
+
+                // Обробник кліку
+                $('[data-action="ua_iptv_test"]').on('hover:enter click', function(e) {
+                    console.log('UA IPTV: Menu clicked');
+                    e.preventDefault();
+                    e.stopPropagation();
+
+                    Lampa.Activity.push({
+                        url: '',
+                        title: 'UA IPTV TEST',
+                        component: 'ua_iptv_test',
+                        page: 1
+                    });
+                });
+
+                console.log('UA IPTV: Menu handler attached');
+            } else {
+                console.error('UA IPTV: Menu not found!');
+            }
+        }, 2000);
+
+        console.log('=== UA IPTV: Plugin initialized ===');
+    });
 
 })();
